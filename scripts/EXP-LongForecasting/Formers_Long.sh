102668a8286f6cdfbe17406de692a182dea15b18
Luca Agostini <ago.luca2002@gmail.com>
2026-06-10 12:44:36 +0200

    Chagne the path in Formers_Long.sh to be run on Kaggle

--- scripts/EXP-LongForecasting/Formers_Long.sh
+++ scripts/EXP-LongForecasting/Formers_Long.sh
@@ -13,7 +13,7 @@ for pred_len in 96 192 336 720
 do
   python -u run_longExp.py \
     --is_training 1 \
-    --root_path ./dataset/ \
+    --root_path /kaggle/input/datasets/lukaagostini/ltsf-benchmark-datasets/ \
     --data_path exchange_rate.csv \
     --model_id exchange_96_$pred_len \
     --model $model_name \
@@ -34,7 +34,7 @@ do
 
   python -u run_longExp.py \
       --is_training 1 \
-      --root_path ./dataset/ \
+      --root_path /kaggle/input/datasets/lukaagostini/ltsf-benchmark-datasets/ \
       --data_path electricity.csv \
       --model_id electricity_96_$pred_len \
       --model $model_name \
@@ -54,7 +54,7 @@ do
 
   python -u run_longExp.py \
     --is_training 1 \
-    --root_path ./dataset/ \
+    --root_path /kaggle/input/datasets/lukaagostini/ltsf-benchmark-datasets/ \
     --data_path traffic.csv \
     --model_id traffic_96_$pred_len \
     --model $model_name \
@@ -75,7 +75,7 @@ do
 
   python -u run_longExp.py \
     --is_training 1 \
-    --root_path ./dataset/ \
+    --root_path /kaggle/input/datasets/lukaagostini/ltsf-benchmark-datasets/\
     --data_path weather.csv \
     --model_id weather_96_$pred_len \
     --model $model_name \
@@ -96,7 +96,7 @@ do
 
   python -u run_longExp.py \
       --is_training 1 \
-      --root_path ./dataset/ \
+      --root_path /kaggle/input/datasets/lukaagostini/ltsf-benchmark-datasets/ \
       --data_path ETTh1.csv \
       --model_id ETTh1_96_$pred_len \
       --model $model_name \
@@ -116,7 +116,7 @@ do
   
   python -u run_longExp.py \
       --is_training 1 \
-      --root_path ./dataset/ \
+      --root_path /kaggle/input/datasets/lukaagostini/ltsf-benchmark-datasets/ \
       --data_path ETTh2.csv \
       --model_id ETTh2_96_$pred_len \
       --model $model_name \
@@ -136,7 +136,7 @@ do
   
   python -u run_longExp.py \
       --is_training 1 \
-      --root_path ./dataset/ \
+      --root_path /kaggle/input/datasets/lukaagostini/ltsf-benchmark-datasets/ \
       --data_path ETTm1.csv \
       --model_id ETTm1_96_$pred_len \
       --model $model_name \
@@ -156,7 +156,7 @@ do
 
   python -u run_longExp.py \
       --is_training 1 \
-      --root_path ./dataset/ \
+      --root_path /kaggle/input/datasets/lukaagostini/ltsf-benchmark-datasets/ \
       --data_path ETTm2.csv \
       --model_id ETTm2_96_$pred_len \
       --model $model_name \
